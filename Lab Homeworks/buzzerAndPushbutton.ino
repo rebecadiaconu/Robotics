03273aaa591a8220a0d89955fce83c580bfade41
Rebeca Diaconu <57195730+rebecadiaconu@users.noreply.github.com>
2019-11-21 18:39:53 +0200

    Update and rename knock_knock.ino to buzzerAndPushbutton.ino

--- Lab Homeworks/buzzerAndPushbutton.ino
+++ Lab Homeworks/buzzerAndPushbutton.ino
@@ -1,45 +1,48 @@
-const int activeBuzzerPin = 8;
+const int activeBuzzerPin = 11;
 const int knockSensor = A0;
 const int buttonPin = 2;
-const int threshold = 430;
+const int threshold = 470 ;
 
 int buzzerState = 0;
 int sensorReading = 0;
 int buttonState = 0;
-int previousButtonValue = 0;
+int previousButtonState = 0;
 
 int ok = 0;
+int buzzerValue = 0;
 int knockTime = 0;
 int lastDebounceTime = 0;
 int debounceDelay = 50;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(activeBuzzerPin, OUTPUT);
+  pinMode(knockSensor, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   sensorReading = analogRead(knockSensor);
-
+ // Serial.println(sensorReading);
   if (sensorReading > threshold) {
         Serial.println("Knock!");
         ok = 1;
+        knockTime = millis();
+        buzzerValue = sensorReading;
   }
-
-  knockTime = millis();
-  if (ok == 1)
-     if (millis() - knockTime > 5000){
-           tone(activeBuzzerPin, sensorReading);
-           buzzerState = 1;
+  
+  if (ok == 1) {
+    if (millis() - knockTime > 5000){
+          tone(activeBuzzerPin, buzzerValue);
+          buzzerState = 1;
         }
+  }
 
   int reading = digitalRead(buttonPin);
   
-  if (reading != previousButtonValue){
+  if (reading != previousButtonState){
     lastDebounceTime = millis();
   }
 
@@ -49,13 +52,13 @@ void loop() {
     }
 
       
-  if (buttonState == HIGH && buzzerState == 1){
+  if (buttonState == LOW && buzzerState == 1){
         noTone(activeBuzzerPin);
         buzzerState = 0;
+        ok = 0;
       }
 
-  previousButtonValue = reading;
+  previousButtonState = reading;
 
-  Serial.println(sensorReading);
   delay(100);
 }
